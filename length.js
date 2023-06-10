472d75c9e59d496dff1f80f6d6a7ed0b48ab079a
Naveen Kushwaha <naveenkushwaha4321@gmail.com>
2023-06-11 00:00:44 +0530

    First commit

---
+++ length.js
@@ -0,0 +1,96 @@
+var input = document.getElementById('input');
+var result = document.getElementById('result');
+var inputType = document.getElementById('inputType');
+var resultType = document.getElementById('resultType');
+var option_from,option_to;
+
+input.addEventListener("keyup",myResult);
+inputType.addEventListener("change",myResult);
+resultType.addEventListener("change",myResult);
+
+
+option_from = inputType.value;
+option_to   = resultType.value;
+
+
+function myResult(){
+
+	option_from = inputType.value;
+	option_to = resultType.value;
+
+
+	if(option_from === "meter" && option_to==="kilometer")
+	{
+		result.value = Number(input.value) * 0.001;
+	}
+	else if(option_from === "meter" && option_to==="Centimeter")
+	{
+		result.value = Number(input.value) * 100;
+
+	}
+	else if(option_from === "meter" && option_to==="meter")
+	{
+		result.value = input.value
+	}
+    else if(option_from === "meter" && option_to==="millimeter")
+    {
+        result.value = Number(input.value) * 1000;
+    }
+
+
+
+	if(option_from === "kilometer" && option_to==="meter")
+	{
+		result.value = Number(input.value) * 1000;
+	}
+	else if(option_from === "kilometer" && option_to==="Centimeter")
+	{
+		result.value = Number(input.value) * 100000;
+	}
+	else if(option_from === "kilometer" && option_to==="kilometer")
+	{
+		result.value = input.value
+	}
+    else if(option_from === "kilometer" && option_to==="millimeter")
+	{
+		result.value = Number(input.value) * 1000000;
+	}
+
+
+
+	if(option_from === "Centimeter" && option_to==="kilometer")
+	{
+		result.value = Number(input.value) * 0.00001;
+	}
+	else if(option_from === "Centimeter" && option_to==="meter")
+	{
+		result.value = Number(input.value) * (0.01);
+	}
+	else if(option_from === "Centimeter" && option_to==="Centimeter")
+	{
+		result.value = input.value
+	}
+    if(option_from === "Centimeter" && option_to==="millimeter")
+	{
+		result.value = Number(input.value) * 10;
+	}
+
+
+
+	if(option_from === "millimeter" && option_to==="meter")
+	{
+		result.value = Number(input.value) * 0.001;
+	}
+	else if(option_from === "millimeter" && option_to==="kilometer")
+	{
+		result.value = Number(input.value) * (0.000001);
+	}
+	else if(option_from === "millimeter" && option_to==="Centimeter")
+	{
+		result.value = Number(input.value) * 0.1;
+	}
+    if(option_from === "millimeter" && option_to==="millimeter")
+	{
+		result.value = input.value
+	}
+}
